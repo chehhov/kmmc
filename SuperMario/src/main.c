cdffa6f9903537c9a8f21847717682b399d9af5a
chehhov <sl2261981@gmail.com>
2026-05-17 12:39:16 +0300

    Расширил структуру TObject, добавив параметры width и height. Добавил новую процедуру InitObject, которая инициализирует персонажа с заданными значениями width и height. В процедуре PutObjectOnMap расширил функционал, чтобы персонаж соответствовал width и height.

--- SuperMario/src/main.c
+++ SuperMario/src/main.c
@@ -9,6 +9,7 @@
 
 typedef struct SObject {
 	float x, y;
+	float width, height;
 } TObject;
 
 char map[mapHeight][mapWidth+1];
@@ -33,14 +34,25 @@ void SetObjectPos(TObject *obj, float xPos, float yPos) {
 	(*obj).y = yPos;
 }
 
+void InitObject(TObject *obj, float xPos, float yPos, float oWidth, float oHeight) {
+	SetObjectPos(obj, xPos, yPos);
+	(*obj).width = oWidth;
+	(*obj).height = oHeight;
+}
+
 void PutObjectOnMap(TObject obj) {
 	int ix = (int)round(obj.x);
 	int iy = (int)round(obj.y);
-	map[iy][ix] = '@';
+	int iWidth = (int)round(obj.width);
+	int iHeight = (int)round(obj.height);
+	
+	for (int i = ix; i < (ix + iWidth); i++) 
+		for (int j = iy; j < (iy + iHeight); j++) 
+			map[j][i] = '@';
 }
 
 int main() {
-	SetObjectPos(&mario, 20, 10);
+	InitObject(&mario, 39, 10, 3, 3);
 	ClearMap();
 	PutObjectOnMap(mario);
 	ShowMap();
